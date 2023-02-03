cf42ef19c5b7f4fd8ac735096a9484900c18b8a1
Daniel <danchan020@gmail.com>
2023-02-02 21:34:33 -0800

    model and repo

--- src/main/java/com/snoozer/backend/model/User.java
+++ src/main/java/com/snoozer/backend/model/User.java
@@ -7,6 +7,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Data
 @Builder
@@ -15,7 +19,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name="_user")
 
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue
     private Integer id;
@@ -23,4 +27,38 @@ public class User {
     private String username;
     private String password;
 
+    //delete authorities later, not needed unless user has roles
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/com/snoozer/backend/repository/UserRepository.java
+++ src/main/java/com/snoozer/backend/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.snoozer.backend.repository;
 
-public interface UserRepository {
+import com.snoozer.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
 }
