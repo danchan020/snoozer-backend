61109008fe1ebe2059ec6b5860253f1f98c95e4d
Daniel <danchan020@gmail.com>
2023-02-06 16:51:57 -0800

    security

--- src/main/java/com/snoozer/backend/controller/UserController.java
+++ src/main/java/com/snoozer/backend/controller/UserController.java
@@ -1,4 +1,29 @@
 package com.snoozer.backend.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final AuthenticationService service;
+    @PostMapping("/signup")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ){
+        return ResponseEntity.ok(service.register(request));
+    }
+
+    @PostMapping("/me")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request
+    ){
+        return ResponseEntity.ok(service.authenticate(request));
+    }
 }
